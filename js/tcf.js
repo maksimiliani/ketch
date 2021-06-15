305e100f27db379bc699212567c6db5c97d16afc
maksimiliani <macseg@gmail.com>
2021-06-15 21:50:50 +0300

    Update tcf.js

--- js/tcf.js
+++ js/tcf.js
@@ -1,8 +1,50 @@
+var tcf_core;
+function populate_sections_nested(jsonObj_ref, h2_title) {
+  var purposes_obj = jsonObj_ref;
+
+  var tcf_section = document.createElement('div');
+  tcf_section.setAttribute('class', 'tcf__section');
+
+  var tcf_h2 = document.createElement('h2');
+  tcf_h2.textContent = h2_title;
+  tcf_section.append(tcf_h2);
+
+  var purposes = $.map(purposes_obj, function(value, index){
+    return [value];
+  });
+
+  for (var i = 0; i < purposes.length; i++) {
+    var tcf_block = document.createElement('div');
+    tcf_block.setAttribute('class', 'tcf_block');
+
+    var tcf_block_title = document.createElement('h4');
+    var tcf_block_p = document.createElement('p');
+    tcf_block_p.setAttribute('class', 'tcf__p');
+
+    var tcf_p_ul = document.createElement('ul');
+    var tcf_p_ul_lis = purposes[i].descriptionLegal.split('\n* ');
+
+    for (var j = 1; j < tcf_p_ul_lis.length; j++) {
+      var listItem = document.createElement('li');
+      listItem.textContent = tcf_p_ul_lis[j];
+      tcf_p_ul.append(listItem);
+    }
+
+    tcf_block_title.textContent = purposes[i].name;
+    tcf_block_p.textContent = tcf_p_ul_lis[0];
+
+    tcf_block.append(tcf_block_title);
+    tcf_block.append(tcf_block_p);
+    tcf_block.append(tcf_p_ul);
+
+    tcf_section.append(tcf_block);
+  }
+  tcf_core.append(tcf_section);
+}
 
 // document ready wrapper
 $(document).ready( function() {
-  var tcf_core = $( "#tcf_core" );
-  var tcf_source;
+  tcf_core = $( "#tcf_core" );
 
   $("#field_lang").change(function(){
   		tcf_core.empty();
@@ -17,47 +59,10 @@ $(document).ready( function() {
         });
 
       function populate_sections(jsonObj) {
-
-        var purposes_obj = jsonObj['purposes'];
-
-        var tcf_section = document.createElement('div');
-        tcf_section.setAttribute('class', 'tcf__section');
-
-        var tcf_h2 = document.createElement('h2');
-        tcf_h2.textContent = "Purposes";
-        tcf_section.append(tcf_h2);
-
-        var purposes = $.map(purposes_obj, function(value, index){
-          return [value];
-        });
-
-        for (var i = 0; i < purposes.length; i++) {
-          var tcf_block = document.createElement('div');
-          tcf_block.setAttribute('class', 'tcf_block');
-
-          var tcf_block_title = document.createElement('h4');
-          var tcf_block_p = document.createElement('p');
-          tcf_block_p.setAttribute('class', 'tcf__p');
-
-          var tcf_p_ul = document.createElement('ul');
-          var tcf_p_ul_lis = purposes[i].descriptionLegal.split('\n* ');
-
-          for (var j = 1; j < tcf_p_ul_lis.length; j++) {
-            var listItem = document.createElement('li');
-            listItem.textContent = tcf_p_ul_lis[j];
-            tcf_p_ul.append(listItem);
-          }
-
-          tcf_block_title.textContent = purposes[i].name;
-          tcf_block_p.textContent = tcf_p_ul_lis[0];
-
-          tcf_block.append(tcf_block_title);
-          tcf_block.append(tcf_block_p);
-          tcf_block.append(tcf_p_ul);
-
-          tcf_section.append(tcf_block);
-        }
-        tcf_core.append(tcf_section);
+        populate_sections_nested(jsonObj['purposes'], "Purposes");
+        populate_sections_nested(jsonObj['specialPurposes'], "Special Purposes");
+        populate_sections_nested(jsonObj['features'], "Features");
+        populate_sections_nested(jsonObj['specialFeatures'], "Special Features");
       }
     });
 });
